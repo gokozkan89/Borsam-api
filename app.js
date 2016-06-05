9392a88d42b62ac78369a08bfbb4f8887371388d
gokozkan89 <gokozkan89@gmail.com>
2016-06-06 01:31:02 +0300

    son

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/borsa', borsa);
 //gokhan
-app.use('/oneri',oneri);
+app.use('/oneri', oneri);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -45,7 +45,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     //res.status(err.status || 500);
     res.status(200).json({
       message: err.message,
@@ -57,7 +57,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   //res.status(err.status || 500);
   res.status(200).json({
     message: err.message,
@@ -65,6 +65,5 @@ app.use(function(err, req, res, next) {
     status: 'hata'
   });
 });
-
 
 module.exports = app;
